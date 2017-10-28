2e3eb4ff4c49566df97b51a7ede4d07119a25fc2
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2017-10-29 00:25:45 +0200

    Ticket aggiornato

--- ing_sw/common/Ticket.java
+++ ing_sw/common/Ticket.java
@@ -7,41 +7,33 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * Struttura contenente i dati relativi ad una singola foto/documento.
+ * Struttura contenente i dati relativi ad un singolo scontrino.
  * @author Riccardo Zaglia (Gruppo 2)
  */
 public class Ticket implements Serializable {
 
     /**
      * Identificatore.
-     * NB: non modificare al di fuori della classe che implementa IDatabase
      */
     public int ID;
 
     /**
-     * URI associato al file del documento (puo' essere un percorso locale o remoto).
-     * L'oggetto URI puo' essere convertito in stringa con il metodo toString.
+     * URI associato alla foto dello scontrino.
      */
-    public URI getFileURI;
+    public URI fileURI;
 
     /**
-     * Titolo del documento
+     * Titolo dello scontrino.
      */
     public String title;
 
     /**
-     * Data del documento.
+     * Data dello scontrino.
      */
     public Date date;
 
     /**
-     * Importo del documento.
+     * Importo dello scontrino.
      */
     public BigDecimal amount;
-
-    /**
-     * Lista di oggetti contenete dati relativi a funzionalita' aggiuntive non compatibili tra gruppi.
-     * E' possibile accedere ai dati con il casting.
-     */
-    public List<Object> opaqueData;
 }
