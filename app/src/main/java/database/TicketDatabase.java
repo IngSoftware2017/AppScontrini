b0ac9c841dd6589d3016a5dc4473563c557c17ef
federicotaschin <32360377+federicotaschin@users.noreply.github.com>
2017-11-08 22:56:41 +0100

    Created the base structure of the database using Room libraries Tell me what do you think about this, i found it simple to use and less prone to errors. Room tutorial: https://developer.android.com/training/data-storage/room/index.html I found useful this tutorial also: https://mediumcom/@ajaysainiofficial/building-database-with-room-persistence-library-ecf7d0b8f3e9 Test methods in progress !Not tested yet!

---
+++ app/src/main/java/database/TicketDatabase.java
@@ -0,0 +1,53 @@
+package database;
+
+import android.arch.persistence.db.SupportSQLiteOpenHelper;
+import android.arch.persistence.room.DatabaseConfiguration;
+import android.arch.persistence.room.InvalidationTracker;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.Database;
+import android.content.Context;
+
+/**
+ * Created by Taschin Federico on 08/11/2017.
+ */
+
+@Database(entities = {TicketEntity.class}, version = 1)
+public abstract class TicketDatabase extends RoomDatabase {
+    private static final String DATABASE_NAME = "ticket-database";
+
+    private static TicketDatabase INSTANCE; //Unique instance of the Database object
+    public abstract DAO ticketDao(); //returns the DAO object that contains query methods. This method is automatically implemented by Room library
+
+    /* Creates the unique instance of the Database object.
+     * @param context not null, Context of the Activity that calls this method.
+     * @return TicketDatabase not null, single instance of the database
+     */
+    public static TicketDatabase getAppDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE =
+                    Room.databaseBuilder(context.getApplicationContext(), TicketDatabase.class, DATABASE_NAME).allowMainThreadQueries().build();
+                            // allow queries on the main thread.
+                            // Don't do this on a real app! See PersistenceBasicSample for an example.
+
+        }
+        return INSTANCE;
+    }
+    //Destroys the Database instance (by setting it to null). getAppDatabase(context) must be called before using the saved instance of Database again
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+
+    //pecifications for this methods will arrive, do not use for now
+    @Override
+    protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
+        return null;
+    }
+    //pecifications for this methods will arrive, do not use for now
+    @Override
+    protected InvalidationTracker createInvalidationTracker() {
+        return null;
+    }
+
+
+}
