d6ae520613d5c4bfd5b84815726ee3d86572f620
Kraktun <30228269+Kraktun@users.noreply.github.com>
2017-11-10 12:51:04 +0100

    up init grid

--- app/src/main/java/com/ing/software/appscontrini2/OCR/OcrHandler.java
+++ app/src/main/java/com/ing/software/appscontrini2/OCR/OcrHandler.java
@@ -7,21 +7,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
-import android.util.SparseArray;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.text.TextBlock;
-import com.google.android.gms.vision.text.TextRecognizer;
-import com.ing.software.appscontrini2.R;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 
 /**
  *
