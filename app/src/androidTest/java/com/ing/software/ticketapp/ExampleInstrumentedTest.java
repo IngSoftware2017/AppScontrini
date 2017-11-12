6f14c0c4eeff358aff8125a0b894eeec362d4bc5
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2017-11-12 12:18:42 +0100

    Ricopiato unit trast

--- app/src/androidTest/java/com/ing/software/ticketapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/ing/software/ticketapp/ExampleInstrumentedTest.java
@@ -1,11 +1,21 @@
 package com.ing.software.ticketapp;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.ing.software.ticketapp.OCR.DataAnalyzer;
+import com.ing.software.ticketapp.OCR.OcrAnalyzer;
+import com.ing.software.ticketapp.OCR.OcrResult;
+import com.ing.software.ticketapp.OCR.OnOcrResultReadyListener;
+import com.ing.software.ticketapp.OCR.OnTicketReadyListener;
+import com.ing.software.ticketapp.common.Ticket;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.concurrent.CountDownLatch;
 
 import static org.junit.Assert.*;
 
@@ -21,6 +31,42 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.ing.software.ticketapp", appContext.getPackageName());
+        OcrAnalyzer ocrAnalyzer = new OcrAnalyzer();
+        while (ocrAnalyzer.initialize(appContext) != 0)
+            Thread.sleep(10);
+        final DataAnalyzer dataAnalyzer = new DataAnalyzer();
+
+
+        // number of Tickets
+        int imgsTot = 1;
+        // Object used to lock current thread until all photos are processed.
+        final CountDownLatch cdl = new CountDownLatch(imgsTot);
+
+
+        // todo: assegnare la bitmap con una foto di uno scontrino.
+        // cancellare questa riga e assegnare una bitmap valida.
+        Bitmap bitmap = Bitmap.createBitmap(1,1, Bitmap.Config.ARGB_8888);
+
+
+        ocrAnalyzer.getOcrResult(bitmap, new OnOcrResultReadyListener() {
+            @Override
+            public void onOcrResultReady(OcrResult result) {
+                dataAnalyzer.getTicket(result, new OnTicketReadyListener() {
+                    @Override
+                    public void onTicketReady(Ticket ticket) {
+
+
+                        //todo: controllare che i dati contenuti in ticket sono corretti.
+
+
+                        //decreases the counter
+                        cdl.countDown();
+                    }
+                });
+            }
+        });
+
+        //Thread lock. If the counter reaches zero, the thread unlocks and the test ends.
+        cdl.await();
     }
 }
