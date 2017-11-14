a0ecf49bdd57fa9eff27c5590651c94b8f1ea683
MarcoOlivieri <marco.olivieri@studenti.unipd.it>
2017-11-14 18:01:31 +0100

    Changed methods in DataManager. Cast of type.

--- app/src/main/java/database/DataManager.java
+++ app/src/main/java/database/DataManager.java
@@ -25,7 +25,7 @@ public class DataManager {
      * @return ID of the created record, -1 if the method fails
      */
     public int addTicket(Ticket ticket){
-        return (int)database.ticketDao().addTicket(toTicketEntity(ticket));
+        return (int)database.ticketDao().addTicket((TicketEntity) ticket);
     }
 
     /**Modifies the values (with the exception of ID) of a ticket stored in the database
@@ -35,7 +35,7 @@ public class DataManager {
     * @return true if the update is executed, false otherwise (i.e. invalid ID)
     */
     public boolean updateTicket(Ticket ticket){
-         return database.ticketDao().updateTicket(toTicketEntity(ticket))>0; //true if at least a ticket is updated
+         return database.ticketDao().updateTicket((TicketEntity) ticket)>0; //true if at least a ticket is updated
     }
 
     /**
@@ -43,37 +43,6 @@ public class DataManager {
      */
     public List<Ticket> getAllTickets(){
         return toTicket(database.ticketDao().getAllTickets());
-    }
-
-    /** Turns a Ticket object into a TicketEntity object
-     * @param ticket Ticket not null
-     * @return an instance of TicketEntity with values from the Ticket received
-     */
-    private TicketEntity toTicketEntity(Ticket ticket){
-        TicketEntity entity = new TicketEntity();
-        entity.setID(ticket.getID());
-        entity.setFileUri(ticket.getFileUri());
-        entity.setAmount(ticket.getAmount());
-        entity.setShop(ticket.getShop());
-        entity.setDate(ticket.getDate());
-        entity.setTitle(ticket.getTitle());
-        return entity;
-    }
-
-    /**
-     * Turns a TicketEntity into a Ticket
-     * @param ticketEntity not null
-     * @return an instance of Ticket with values from the TicketEntity received
-     */
-    private Ticket toTicket(TicketEntity ticketEntity){
-        Ticket ticket = new Ticket();
-        ticket.setID(ticketEntity.getID());
-        ticket.setFileUri(ticketEntity.getFileUri());
-        ticket.setAmount(ticketEntity.getAmount());
-        ticket.setShop(ticketEntity.getShop());
-        ticket.setDate(ticketEntity.getDate());
-        ticket.setTitle(ticketEntity.getTitle());
-        return ticket;
     }
 
     /**
@@ -84,7 +53,7 @@ public class DataManager {
     private List<Ticket> toTicket(List<TicketEntity> ticketEntities){
         ArrayList<Ticket> tickets = new ArrayList<Ticket>();
         for(TicketEntity entity : ticketEntities){
-            tickets.add(toTicket(entity));
+            tickets.add((TicketEntity) entity);
         }
         return tickets;
     }
