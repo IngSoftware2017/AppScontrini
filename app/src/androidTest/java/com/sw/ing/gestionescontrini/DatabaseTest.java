f63459d80fd43b2c2fd7a8e175f04223cee30d0d
federicotaschin <32360377+federicotaschin@users.noreply.github.com>
2017-11-14 21:31:17 +0100

    Defined specifications for DAO interface

--- app/src/androidTest/java/com/sw/ing/gestionescontrini/DatabaseTest.java
+++ app/src/androidTest/java/com/sw/ing/gestionescontrini/DatabaseTest.java
@@ -6,10 +6,13 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.util.Date;
 
 import database.DAO;
 import database.Database;
@@ -30,14 +33,17 @@ public class DatabaseTest {
         Context context = InstrumentationRegistry.getTargetContext();
         database = Room.inMemoryDatabaseBuilder(context, Database.class).build();
 
-        Method method = Database.class.getDeclaredMethod("ticketDAO()");
+        Method method = Database.class.getDeclaredMethod("ticketDao");
         method.setAccessible(true);
         ticketDAO = (DAO) method.invoke(database, null);
     }
 
+    @Test
     public void insertTicketTest(){
         TicketEntity ticket = new TicketEntity();
         ticket.setShop("Decathlon");
+        ticket.setAmount(new BigDecimal(12));
+        ticket.setDate(new Date(1996,10,12));
         ticketDAO.addTicket(ticket);
     }
 }
