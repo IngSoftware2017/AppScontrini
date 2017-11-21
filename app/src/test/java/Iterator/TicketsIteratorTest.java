82269fd045a5d7e50fd3296815d1021daea4312b
mascotto91 <32366159+mascotto91@users.noreply.github.com>
2017-11-21 12:04:12 +0100

    Improving the test of the add and get methods. Necessaries MOCK TEST for the Bitmap generation!
    I created two different ways to add Bitmap files, the first auto
    generate the file, thw second load it from path.

--- app/src/test/java/Iterator/TicketsIteratorTest.java
+++ app/src/test/java/Iterator/TicketsIteratorTest.java
@@ -1,6 +1,7 @@
 package Iterator;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 import org.hamcrest.Matcher;
 import org.junit.Test;
@@ -17,33 +18,76 @@ import static org.junit.Assert.*;
  */
 public class TicketsIteratorTest {
 
-    Bitmap img;
+    Bitmap img_Bmp;
     TicketsIterator<Bitmap> TicketsList = new TicketsIterator();
 
     /*
-    How can i insert a new bitmap object in the  TicketsIterator?
+     * Test for the add e get methods, check if the element just insert it's the same of the one just extract.
      */
-
     @Test
-    public void add() throws Exception {
-        // assertTrue(TicketsList.add(img) == TicketsList.get(img));
+    public void Add_n_Get() throws Exception {
+        int i = 0;
+        int minHeight = 100, maxHeight = 500;
+        int minWeight = 100, maxWeight = 500;
+        Random numR = new Random();
+        int numIMG = 50;
+        Bitmap[] IMG_Generated;
+        IMG_Generated = new Bitmap[100];
+        String path_IMG = "C:\\Users\\matteo.mascotto\\Pictures\\Tickets\\";
+
+        // Generate numIMG of imagine to insert into the Iterator and it save each one of it into an
+        // Bitmap array usefull for testing of the get method
+        while (i <= numIMG) {
+            // Generation of the fake Ticket Bitmap
+            try {
+                /*
+                 * Two different ways to auto-generation fake imagine for testing. Necessary Mock Test!!
+                 */
+                // 1
+                img_Bmp = Bitmap.createBitmap(numR.nextInt(maxHeight - minHeight + 1) + minHeight, numR.nextInt(maxWeight - minWeight + 1) + minWeight, Bitmap.Config.ARGB_4444);
+                // 2
+                img_Bmp = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+
+                /*
+                 * Extraction of Bitmap from real file and bitmap generation
+                 */
+                img_Bmp = BitmapFactory.decodeFile(path_IMG + "00");
+
+                IMG_Generated[i] = img_Bmp;
+
+            } catch (Exception e) {
+                // Print the cause of the error just generated
+                e.getCause().printStackTrace();
+            }
+
+            // Addition of the imagine just created
+            TicketsList.add(img_Bmp);
+
+            i++;
+        }
+
+        // Test if the imagine inserted it is correct
+        while (i <= numIMG) {
+            assertTrue(IMG_Generated[i] == TicketsList.get(IMG_Generated[i]));
+            i++;
+        }
     }
 
     @Test
-    public void get() throws Exception {
+    public void remove() throws Exception {
     }
 
     @Test
     public void iterate_throught_TicketsIterator_in_right_order() {
 
-        int i = 0, count = 0;
+        int i = 0;
         int minHeight = 100, maxHeight = 500;
         int minWeight = 100, maxWeight = 500;
         Random numR = new Random();
 
         while (i <= 10) {
-            img = Bitmap.createBitmap(numR.nextInt(maxHeight - minHeight + 1) + minHeight, numR.nextInt(maxWeight - minWeight + 1) + minWeight, Bitmap.Config.ARGB_4444);
-            TicketsList.add(img);
+            img_Bmp = Bitmap.createBitmap(numR.nextInt(maxHeight - minHeight + 1) + minHeight, numR.nextInt(maxWeight - minWeight + 1) + minWeight, Bitmap.Config.ARGB_4444);
+            TicketsList.add(img_Bmp);
             i++;
         }
 
