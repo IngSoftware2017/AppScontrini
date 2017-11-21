b70e75b63fff1a1189a61b3b468cc7c1e93847e0
Kraktun <30228269+Kraktun@users.noreply.github.com>
2017-11-21 16:52:20 +0100

    Fix test

--- app/src/androidTest/java/com/ing/software/ticketapp/OCR/OcrInstrumentedTests.java
+++ app/src/androidTest/java/com/ing/software/ticketapp/OCR/OcrInstrumentedTests.java
@@ -3,13 +3,23 @@
  */
 
 package com.ing.software.ticketapp.OCR;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import static junit.framework.Assert.*;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
-class OcrInstrumentedTests {
+public class OcrInstrumentedTests {
 
+    @Test
+    public void useAppContext() throws Exception {
+        //There must be a test
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.ing.software.ticketapp", appContext.getPackageName());
+    }
 }
