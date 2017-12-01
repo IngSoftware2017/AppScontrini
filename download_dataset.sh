3a40635e85463c8ece7f26d5a359d24c6eaf79f0
Riccardo Zaglia <riccardo.zaglia5@gmail.com>
2017-12-01 23:07:41 +0100

    up2

--- download_dataset.sh
+++ download_dataset.sh
@@ -2,8 +2,8 @@
 
 # Download a dummy photo
 GDRIVE_DIR='GDrive/Ingegneria del software/DataSet/photos'
-mkdir -p $GDRIVE_DIR
-pushd $GDRIVE_DIR
+mkdir -p "$GDRIVE_DIR"
+pushd "$GDRIVE_DIR"
 	wget --no-check-certificate 'https://docs.google.com/uc?id=11bZwcT1EkxaVor0Ux8Houl9JFD8Tpkoc&export=download' -O '0.jpg'
 	ls
 popd
