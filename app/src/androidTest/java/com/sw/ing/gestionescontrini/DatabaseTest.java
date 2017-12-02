fdc14e54231183eef1e98165dc78ed4aa7793e7c
federicotaschin <32360377+federicotaschin@users.noreply.github.com>
2017-12-02 12:05:07 +0100

    Fixed foreign key violation, added tests
    Fixed foreig key constraints violations due to missing Mission ans Person.
    
    modifies setEntities():
      -renamed to createTicket1AndInsert(): now the method creates only
        ticket1 and the related Mission and Person. and links them
    if other Ticket are needed for tests, a method createTicket[num]AndInsert() should be created. This method should take care of the creation of the related Mission and Person, or it can use the same for testTicket1
    
    Tests added:
    -updateNotEmptyTicketTest() simple update test on a field
    -updateTicketShouldNullEmptyFields() test the update on a ticket with some null fields.
    -updateNullTicketIDShouldReturnZero()
    -updateInvalidTicketIDShouldReturnZero()
    -deleteExistingTicketTest()

--- app/src/androidTest/java/com/sw/ing/gestionescontrini/DatabaseTest.java
+++ app/src/androidTest/java/com/sw/ing/gestionescontrini/DatabaseTest.java
@@ -4,11 +4,9 @@ import android.arch.persistence.room.Room;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.Log;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -19,6 +17,8 @@ import java.util.Date;
 
 import database.DAO;
 import database.Database;
+import database.Mission;
+import database.Person;
 import database.Ticket;
 
 /**
@@ -30,36 +30,49 @@ public class DatabaseTest {
 
      static Database database;
      static DAO ticketDAO;
-     static Ticket testTicket1, testTicket2;
+     static Ticket testTicket1;
+    // static Ticket testTicket2;
+     static Mission mission;
+     static Person person;
 
     @Before
-    public static void setDatabase() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public void setDatabaseAndCreateTickets() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Context context = InstrumentationRegistry.getTargetContext();
         database = Room.inMemoryDatabaseBuilder(context, Database.class).build();
 
         Method method = Database.class.getDeclaredMethod("ticketDao");
         method.setAccessible(true);
         ticketDAO = (DAO) method.invoke(database, null);
+        createTicket1AndInsert();
     }
 
-    @Before
-    public void setTicket(){
+
+    public void createTicket1AndInsert(){
+        person = new Person();
+        person.setName("Albert");
+
+        person.setID((int) ticketDAO.addPerson(person));
+
+        mission = new Mission();
+        mission.setPersonID(person.getID());
+        mission.setID((int) ticketDAO.addMission(mission));
+
         testTicket1 = new Ticket();
         testTicket1.setAmount(new BigDecimal(12));
         testTicket1.setDate(new Date(1996,10,12));
         testTicket1.setShop("Decathlon");
         testTicket1.setTitle("Football shoes");
-        testTicket1.setMissionID(1);
+        testTicket1.setMissionID(mission.getID());
 
-        testTicket2 = new Ticket();
+        /*testTicket2 = new Ticket();
         testTicket2.setAmount(new BigDecimal(12));
         testTicket2.setDate(new Date(1966,11,12));
         testTicket2.setShop("wwww");
         testTicket2.setTitle("wwwwwwwww shoes");
-        testTicket2.setMissionID(2);
+        testTicket2.setMissionID(mission.getID());*/
 
-        ticketDAO.addTicket(testTicket1);
-        ticketDAO.addTicket(testTicket2);
+        testTicket1.setID((int) ticketDAO.addTicket(testTicket1));
+        //ticketDAO.addTicket(testTicket2);
     }
 
     @Test
@@ -79,17 +92,38 @@ public class DatabaseTest {
         Assert.assertTrue(ticketDAO.getAllTickets().get(0).getShop().equals(testTicket1.getShop()));
     }
     @Test
-    public void updateTicketTest(){
-
+    public void updateNotEmptyTicketTest(){
+        testTicket1.setAmount(new BigDecimal(12));
+        ticketDAO.updateTicket(testTicket1);
+        Assert.assertTrue(ticketDAO.getAllTickets().get(0).getAmount().equals(testTicket1.getAmount()));
     }
 
+    @Test
+    public void updateTicketShouldNullEmptyFields(){
+        Ticket emptyTicket = new Ticket();
+        emptyTicket.setID(testTicket1.getID());
+        emptyTicket.setMissionID(testTicket1.getMissionID());
+        emptyTicket.setTitle("modified value");
+        ticketDAO.updateTicket(emptyTicket);
+        Assert.assertNull(emptyTicket.getAmount());
+    }
+
+    @Test
+    public void updateNullTicketIDShouldReturnZero(){
+        Ticket ticket = new Ticket();
+        Assert.assertTrue(0 == ticketDAO.updateTicket(ticket));
+    }
+
+    @Test
+    public void updateInvalidTicketIDShouldReturnZero(){
+        Ticket ticket = new Ticket();
+        ticket.setID(123);
+        Assert.assertTrue(0 == ticketDAO.updateTicket(ticket));
+    }
 
     @Test
     public void deleteExistingTicketTest(){
-        Ticket testTicketDelete = new Ticket();
-        testTicketDelete.setID((int)ticketDAO.addTicket(testTicketDelete));
-        int id = testTicketDelete.getID();
-        assert (id == ticketDAO.deleteTicket(testTicketDelete.getID()));
+        Assert.assertTrue(ticketDAO.deleteTicket(testTicket1.getID())==1);
     }
     //TODO test with invalid ID, test with null ID (deleteTicket pre-conditions accept null)
 
