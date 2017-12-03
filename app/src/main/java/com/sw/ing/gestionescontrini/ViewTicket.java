fe13edf298c1bb40a02bcc04cc7255709aee9008
MarcoOlivieri <marco.olivieri@studenti.unipd.it>
2017-12-03 18:06:43 +0100

    Added ViewTicketActivity

---
+++ app/src/main/java/com/sw/ing/gestionescontrini/ViewTicket.java
@@ -0,0 +1,13 @@
+package com.sw.ing.gestionescontrini;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewTicket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_ticket);
+    }
+}
