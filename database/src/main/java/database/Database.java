3857d84d1bd2e09dbe80aba8d20b7cdf6f78c00d
mascotto91 <32366159+mascotto91@users.noreply.github.com>
2017-12-06 16:34:33 +0100

    Add Title and Description to the Mission Entity and link the mission creation to the DB

--- database/src/main/java/database/Database.java
+++ database/src/main/java/database/Database.java
@@ -1,17 +1,19 @@
 package database;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.db.SupportSQLiteOpenHelper;
 import android.arch.persistence.room.DatabaseConfiguration;
 import android.arch.persistence.room.InvalidationTracker;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
 
 /**
  * Created by Taschin Federico on 08/11/2017.
  */
 
-@android.arch.persistence.room.Database(entities = {Ticket.class, Mission.class, Person.class}, version = 1)
+@android.arch.persistence.room.Database(entities = {Ticket.class, Mission.class, Person.class}, version = 3)
 public abstract class Database extends RoomDatabase {
 
     private static Database INSTANCE; //Unique instance of the Database object
@@ -24,10 +26,25 @@ public abstract class Database extends RoomDatabase {
     public static Database getAppDatabase(Context context) {
         if (INSTANCE == null) {
             INSTANCE = Room.databaseBuilder(context.getApplicationContext(), Database.class, Constants.DATABASE_NAME)
-                            .allowMainThreadQueries().build();
+                            .allowMainThreadQueries().addMigrations(MIGRATION_1_2, MIGRATION_2_3).build();
         }
         return INSTANCE;
     }
+
+    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE Ticket ADD COLUMN Title STRING");
+        }
+    };
+
+    static final Migration MIGRATION_2_3 = new Migration(2, 3) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE Ticket ADD COLUMN Description STRING");
+        }
+    };
+
     /**Destroys the Database instance (by setting it to null). getAppDatabase(context) must be called before using the saved instance of Database again
     **/
     public static void destroyInstance() {
