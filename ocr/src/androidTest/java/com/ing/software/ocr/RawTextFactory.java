3685ebd0d9253fe73e076621afc31a81417f0233
Kraktun <30228269+Kraktun@users.noreply.github.com>
2017-12-14 17:05:46 +0100

    Added constructors to RawTextFactory

--- ocr/src/androidTest/java/com/ing/software/ocr/RawTextFactory.java
+++ ocr/src/androidTest/java/com/ing/software/ocr/RawTextFactory.java
@@ -11,7 +11,7 @@ import com.ing.software.ocr.OcrObjects.RawText;
 import java.util.List;
 
 /**
- *
+ * Class to create RawTexts
  */
 
 class RawTextFactory {
@@ -31,8 +31,10 @@ class RawTextFactory {
         @Override
         public Point[] getCornerPoints() {
             Point a = new Point(rect.left, rect.top);
-            Point b = new Point(rect.right, rect.bottom);
-            return new Point[]{a, b};
+            Point b = new Point(rect.right, rect.top);
+            Point c = new Point(rect.left, rect.bottom);
+            Point d = new Point(rect.right, rect.bottom);
+            return new Point[]{a, b, c, d};
         }
 
         @Override
@@ -42,5 +44,21 @@ class RawTextFactory {
     };
     RawImage rawImage = new RawImage(image);
     return new RawText(text, rawImage);
+    }
+
+    static RawText getRawText(final String value, final Rect rect) {
+        Bitmap image = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+        return getRawText(value, rect, image);
+    }
+
+    static RawText getRawText(final String value) {
+        Bitmap image = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+        Rect rect = new Rect(10,10,30,50);
+        return getRawText(value, rect, image);
+    }
+
+    static RawText getRawText(final String value, Bitmap image) {
+        Rect rect = new Rect(10,10,30,50);
+        return getRawText(value, rect, image);
     }
 }
