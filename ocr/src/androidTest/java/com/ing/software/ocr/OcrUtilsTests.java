7b3bd746888e081d6723e269f623bd2a329fd527
Kraktun <30228269+Kraktun@users.noreply.github.com>
2018-01-03 16:14:23 +0100

    Fix test

--- ocr/src/androidTest/java/com/ing/software/ocr/OcrUtilsTests.java
+++ ocr/src/androidTest/java/com/ing/software/ocr/OcrUtilsTests.java
@@ -1,6 +1,7 @@
 package com.ing.software.ocr;
 
 import android.graphics.Bitmap;
+import android.graphics.Rect;
 import android.graphics.RectF;
 
 import org.junit.Test;
@@ -13,19 +14,12 @@ import static junit.framework.Assert.assertEquals;
 
 public class OcrUtilsTests {
 
-    @Test
-    public void getPreferredGrid16x9() throws Exception {
-        Bitmap bitmap = Bitmap.createBitmap(90, 160, Bitmap.Config.ARGB_8888);
-        String expectedGrid = "16x9";
-        String resultGrid = OcrUtils.getPreferredGrid(bitmap);
-        assertEquals(expectedGrid, resultGrid);
-    }
 
     @Test
     public void extendRect() throws Exception {
-        RectF source = new RectF(10,10,60,60);
-        RectF target = OcrUtils.extendRect(source, 100, 100);
-        RectF expected = new RectF(0, 0, 85, 85);
+        Rect source = new Rect(10,10,60,60);
+        Rect target = OcrUtils.extendRect(source, 100, 100);
+        Rect expected = new Rect(0, 0, 85, 85);
         assertEquals(expected.left, target.left);
         assertEquals(expected.top, target.top);
         assertEquals(expected.right, target.right);
@@ -34,9 +28,9 @@ public class OcrUtilsTests {
 
     @Test
     public void extendRectOnlyW() throws Exception {
-        RectF source = new RectF(10,10,60,60);
-        RectF target = OcrUtils.extendRect(source, 0, 100);
-        RectF expected = new RectF(0, 10, 85, 60);
+        Rect source = new Rect(10,10,60,60);
+        Rect target = OcrUtils.extendRect(source, 0, 100);
+        Rect expected = new Rect(0, 10, 85, 60);
         assertEquals(expected.left, target.left);
         assertEquals(expected.top, target.top);
         assertEquals(expected.right, target.right);
@@ -45,9 +39,9 @@ public class OcrUtilsTests {
 
     @Test
     public void extendRectOnlyH() throws Exception {
-        RectF source = new RectF(10,10,60,60);
-        RectF target = OcrUtils.extendRect(source, 100, 0);
-        RectF expected = new RectF(10, 0, 60, 85);
+        Rect source = new Rect(10,10,60,60);
+        Rect target = OcrUtils.extendRect(source, 100, 0);
+        Rect expected = new Rect(10, 0, 60, 85);
         assertEquals(expected.left, target.left);
         assertEquals(expected.top, target.top);
         assertEquals(expected.right, target.right);
@@ -56,9 +50,9 @@ public class OcrUtilsTests {
 
     @Test
     public void extendRect2() throws Exception {
-        RectF source = new RectF(50,50,100,80);
-        RectF target = OcrUtils.extendRect(source, 100, 100);
-        RectF expected = new RectF(25, 35, 125, 95);
+        Rect source = new Rect(50,50,100,80);
+        Rect target = OcrUtils.extendRect(source, 100, 100);
+        Rect expected = new Rect(25, 35, 125, 95);
         assertEquals(expected.left, target.left);
         assertEquals(expected.top, target.top);
         assertEquals(expected.right, target.right);
