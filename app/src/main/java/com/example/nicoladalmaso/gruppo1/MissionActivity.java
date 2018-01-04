70f16cfd341fc62fef074393f330006e6e56f0a5
mascotto91 <32366159+mascotto91@users.noreply.github.com>
2018-01-04 11:18:19 +0100

    Develop deletePerson function

--- app/src/main/java/com/example/nicoladalmaso/gruppo1/MissionActivity.java
+++ app/src/main/java/com/example/nicoladalmaso/gruppo1/MissionActivity.java
@@ -32,6 +32,10 @@ import database.MissionEntity;
 import database.PersonEntity;
 import database.TicketEntity;
 
+/**
+ * Modified: Develop of deletePerson function
+ * @author matteo.mascotto on 04/01/2018
+ */
 public class MissionActivity extends AppCompatActivity {
 
     public DataManager DB;
@@ -52,7 +56,7 @@ public class MissionActivity extends AppCompatActivity {
         personID = intent.getExtras().getInt("personID");
         String personName = intent.getExtras().getString("personName");
         //TODO set the title right using string.xml(like personName+" 's missions")
-        setTitle(personName+"- Missioni");
+        setTitle(personName + " - Missioni");
         Log.d("idPersona:", personID+" "+personName);
         Log.d("PersonID", ""+personID);
         String path = getExternalFilesDir(Environment.DIRECTORY_PICTURES).toString();
@@ -91,7 +95,7 @@ public class MissionActivity extends AppCompatActivity {
         switch (item.getItemId()) {
 
             case (R.id.action_deletePerson):
-                //TODO:DELETE THE PERSON
+                deletePerson();
                 break;
 
             case (R.id.action_editPerson):
@@ -174,5 +178,41 @@ public class MissionActivity extends AppCompatActivity {
             noMissions.setVisibility(View.INVISIBLE);
         }
 
+    }
+
+
+    /**
+     * Delete the Person and all the Missions associate to him
+     *
+     * @author matteo.mascotto on 04/01/2018
+     */
+    public void deletePerson(){
+        AlertDialog.Builder toast = new AlertDialog.Builder(MissionActivity.this);
+        // Dialog
+        toast.setMessage(context.getString(R.string.deletePersonToast))
+                .setTitle(context.getString(R.string.deleteTitle));
+        // Positive button
+        toast.setPositiveButton(context.getString(R.string.buttonDelete), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                List<MissionEntity> list = DB.getMissionsForPerson(personID);
+                for(int i = 0; i < list.size(); i++){
+                    DB.deleteMission((int) list.get(i).getID());
+                }
+                DB.deletePerson(personID);
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+        // NOPE
+        toast.setNegativeButton(context.getString(R.string.cancel), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //Nothing to do
+            }
+        });
+        //Show toast
+        AlertDialog alert = toast.show();
+        Button nbutton = alert.getButton(DialogInterface.BUTTON_POSITIVE);
+        nbutton.setTextColor(Color.parseColor("#2196F3"));
     }
 }
